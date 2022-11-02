2a90f4c269399a519ec7d95a4e419b58939f4794
hyun <khj951213@gmail.com>
2022-11-02 23:19:44 +1100

    ?

--- Algorithm/Algorithm/Program.cs
+++ Algorithm/Algorithm/Program.cs
@@ -11,7 +11,6 @@ namespace Algorithm
         private static int size;
         private static int[]? numArray;
         private static int[]? sortResult;
-        private static int test;
 
 
         public static void Main(string[] args)
